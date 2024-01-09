4d70109524d105c7b59195d304b52feef49493d0
jennyg0 <jen2348@hotmail.com>
2024-01-09 18:12:18 -0500

    add page for received cards

---
+++ src/components/send-card/NFTGallery.tsx
@@ -0,0 +1,129 @@
+import { useEffect, useState } from 'react';
+import { CARD_CONTRACT_ADDRESS, CARD_CONTRACT_ABI } from '../../utils/constants';
+import { useAccount, useContractRead } from 'wagmi';
+import NotConnected from './NotConnected';
+
+type NFTMetadata = {
+  tokenId: string;
+  metadata: {
+    name: string;
+    description: string;
+    image: string;
+    attributes: Array<{
+      trait_type: string;
+      value: string;
+    }>;
+  };
+};
+
+const NFTGallery = () => {
+  const [nfts, setNfts] = useState<NFTMetadata[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [currentTokenId, setCurrentTokenId] = useState('');
+  const { isConnected, address } = useAccount();
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+    return () => setIsMounted(false); // cleanup function
+  }, []);
+
+  const nftContractConfig = {
+    address: CARD_CONTRACT_ADDRESS,
+    abi: CARD_CONTRACT_ABI,
+  };
+
+  const { data: balanceData } = useContractRead({
+    ...nftContractConfig,
+    functionName: 'balanceOf',
+    args: [address],
+  });
+  const balance = balanceData ? Number(balanceData) : 0;
+
+  const { data: tokenIdData } = useContractRead({
+    ...nftContractConfig,
+    functionName: 'tokenOfOwnerByIndex',
+    args: [address, currentIndex],
+    watch: isConnected && currentIndex < balance,
+  });
+
+  const { data: tokenUriData } = useContractRead({
+    ...nftContractConfig,
+    functionName: 'tokenURI',
+    args: currentTokenId ? [currentTokenId] : [],
+    enabled: currentTokenId !== '',
+    watch: isConnected && currentTokenId !== '',
+  });
+
+  useEffect(() => {
+    if (tokenIdData) {
+      setCurrentTokenId(tokenIdData.toString());
+    }
+  }, [tokenIdData]);
+
+  useEffect(() => {
+    const fetchMetadata = async () => {
+      if (tokenUriData && typeof tokenUriData === 'string') {
+        try {
+          const response = await fetch(tokenUriData.replace('ipfs://', 'https://ipfs.io/ipfs/'));
+          const metadata = await response.json();
+          if (isMounted) {
+            setNfts((prev) => {
+              const isAlreadyAdded = prev.some((nft) => nft.tokenId === currentTokenId);
+              if (isAlreadyAdded) {
+                return prev;
+              }
+              return [...prev, { tokenId: currentTokenId, metadata }];
+            });
+            setCurrentTokenId('');
+          }
+        } catch (error) {
+          console.error('Error fetching metadata:', error);
+        } finally {
+          if (isMounted) {
+            if (currentIndex < Number(balanceData) - 1) {
+              setCurrentIndex((prev) => prev + 1);
+            } else {
+              setLoading(false);
+            }
+          }
+        }
+      }
+    };
+
+    fetchMetadata();
+  }, [tokenUriData, currentTokenId, currentIndex, balanceData, isMounted]);
+
+  if (loading) {
+    return <div>Loading NFTs...</div>;
+  }
+
+  return (
+    <>
+      {isMounted && isConnected ? (
+        <div className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {nfts ? (
+            nfts.map((nft, index) => (
+              <div key={index} className="card card-bordered">
+                <figure>
+                  <img src={nft.metadata.image} alt={`NFT ${index + 1}`} className="rounded-xl" />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">From: add name here </h2>
+                  <p>{nft.metadata.attributes[0].value}</p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>no cards yet!</p>
+          )}
+        </div>
+      ) : (
+        <NotConnected />
+      )}
+    </>
+  );
+};
+export default NFTGallery;
